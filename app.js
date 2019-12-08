48bebb22f99915e4cd88faeb56acea47135b1183
Sean Rose <sean1rose@gmail.com>
2019-12-07 20:27:04 -0800

    Test route 418

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ app.get('/', (req, res) => {
 	res.send(`This is the express app on port ${port}. Locally available in sean1rose repo!`);
 })
 
+app.get('/fouroneeight', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
